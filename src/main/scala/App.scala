d094b5d39422a2bc9b9dbc3bb3049a1ff304107e
ismail <ismail@Mac.lan>
2026-01-13 15:50:30 +0100

    Chapter 2 : Option , Some , None

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -18,4 +18,33 @@ object Main extends App {
   val t = mathlib.tuple_constuct(1, "Ismail", true)
   val (t1, t2, t3) = mathlib.tuple_constuct(1, "Ismail", true)
   println(t2)
+
+  //Options , Some , None
+
+  val CountriesCapital : Map[String, String]= Map(
+    "Maroc" -> "Rabat",
+    "Tunisie" -> "Tunis",
+    "France" -> "Paris"
+  )
+
+  println("********************************************")
+  println("\n")
+  println("Maroc : " + CountriesCapital.get("Maroc"))
+  println("Tunisie : " + CountriesCapital.get("Tunisie"))
+  println("France : " + CountriesCapital.get("France"))
+  println("Algerie : " + CountriesCapital.get("Algerie"))
+  println("\n")
+
+  println("\n")
+  println("Maroc : " + CountriesCapital.get("Maroc").get)
+  println("Tunisie : " + CountriesCapital.get("Tunisie").getOrElse("Unkown Capital"))
+  println("France : " + CountriesCapital.get("France").getOrElse("Unkown Capital"))
+  println("Algerie : " + CountriesCapital.get("Algerie").getOrElse("Unkown Capital"))
+  println("\n")
+
+  val input_email : List[String] = List("ismail@gmail.com", null, "", "true@email.com")
+  def toOpt(input_feild: String) : Option[String] = Option(input_feild).filter(_.nonEmpty)
+  val emails : List[Option[String]]= input_email.map((x: String) => toOpt(x))
+  
+  emails.foreach(email => println(email.getOrElse("Unknown")))
 }
